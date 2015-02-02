a0aa76487aa14a697533c5cecd37d785e99c0081
Hackbright Student <info@hackbrightacademy.com>
2015-02-01 16:34:18 -0800

    corrected errors

--- fibsetmax.js
+++ fibsetmax.js
@@ -1,23 +1,19 @@
-var maxNumber = function fibSet(max){
-   if (max > 1){
-        var fibList = [1];
-        var n = 1;
-        while (n < max){
-            fibList.push(n);
-            n = ((fibList[fibList.length-1]) + (fibList[fibList.length-2]));
-       {
-        for (var i=0; i < fibList.length; i++){
-            if (i % 2 === 0){
-                console.log(i);
-            }
+///Return list of fibonacci numbers going up to but not beyond max.
+function fibSet(max){
+    if (max > 1){
+        var fib_list = [1];
+        var current_fib = 1;
+        while (current_fib < max) {
+            fib_list.push(current_fib);
+            current_fib = ((fib_list[(fib_list.length)-1]) + (fib_list[(fib_list.length)-2]));
         }
-       }
     }
-   
-   }
-    else {
+    for (var i=0; i < fib_list.length; i++){
+        if (((fib_list[i]) % 2) === 0){
+            console.log(fib_list[i])
+        }
+        else {
         console.log("Not even");
+        }
     }
-};
-
-
+}
